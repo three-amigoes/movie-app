52a331c46b5d112bbb153c4ec7ec2551b857cbab
Ashwin <ashwin.singh.087@gmail.com>
2021-07-15 22:01:52 -0400

    got firebase working somehow

--- src/Components/FavoriteList.js
+++ src/Components/FavoriteList.js
@@ -1,16 +1,47 @@
 import firebase from "./firebase";
+import { useState, useEffect } from 'react'
 
 const FavoriteList = () => {
     const dbRef = firebase.database().ref();
-    dbRef.on('value', (data) => {
-        console.log(data.val());
-    })
-
-    return(
-        <div>
+    const [fireBaseList, setFireBaseList] = useState([]);
+    
+    useEffect( () => {
+        dbRef.on('value', (response) => {
+            const data = response.val()
+            const pseudoFBList = []
             
-        </div>
+            for (let key in data) {
+                pseudoFBList.push({key: key, fireBaseInfo: data[key]} )
+            }
+
+            console.log(pseudoFBList);
+            setFireBaseList(pseudoFBList)
+        })
+    }, [])
+
+
+    
+    return (
+        <ul>
+            {
+                fireBaseList.map( (item) => {
+                    return(
+                        <li>{item.fireBaseInfo.title}</li>
+                    )
+                })
+            }
+        </ul>
     )
 }
 
 export default FavoriteList;
+
+    // const [fbList, setFbList] = useState([])
+    // const dbRef = firebase.database().ref();
+    
+    // useEffect( () => {
+    //     dbRef.on('value', (data) => {
+    //         setFbList(data.val());
+    //         console.log(data.val());
+    //     })
+    // }, [])
