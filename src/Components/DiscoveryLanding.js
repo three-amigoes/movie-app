bb7154086036db1cbd9edf9c2ff65654c24d4c64
Shaun S <shaunseeram@hotmail.com>
2023-01-16 10:58:46 -0500

    Fixed proxy issue

--- src/Components/DiscoveryLanding.js
+++ src/Components/DiscoveryLanding.js
@@ -7,18 +7,19 @@ import Footer from '../Components/Footer'
 const DiscoveryLanding = (props) => {
     props.setter(false);
 
-    const discoverURL = 'https://api.themoviedb.org/3/discover/movie/'; //Returns popular movie, doesn't take user query.
-    const apiKey = 'a0e32a4a0c009553ac6020779811cc03';
+    // const discoverURL = 'https://api.themoviedb.org/3/discover/movie/'; //Returns popular movie, doesn't take user query.
+    // const apiKey = 'a0e32a4a0c009553ac6020779811cc03';
     const [discoveryResults, setDiscoveryResults] = useState([])
 
     useEffect( () => {
-        const url = new URL('https://proxy.hackeryou.com');
-        url.search = new URLSearchParams({
-            reqUrl: discoverURL,
-            'params[api_key]': apiKey,
-            'params[adult]': false
-        });
-        fetch(url)
+        // const url = new URL('https://proxy.hackeryou.com');
+        // url.search = new URLSearchParams({
+        //     reqUrl: discoverURL,
+        //     'params[api_key]': apiKey,
+        //     'params[adult]': false
+        // });
+        // fetch(url);
+        fetch("https://api.themoviedb.org/3/discover/movie/?api_key=a0e32a4a0c009553ac6020779811cc03&adult=false")
         .then( (rawData) => {
         return rawData.json();
         }).then( (jsonData) => {
